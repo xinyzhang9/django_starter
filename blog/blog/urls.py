8cb77a0cff45ecabf9f09e50815d2e3c817252fa
Xinyang <xinyzhang9@gmail.com>
2017-04-27 15:36:12 -0700

    working with images

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from posts import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',views.home)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=255)
+	pub_date = models.DateTimeField()
+	image = models.ImageField(upload_to='media/')
+	body = models.TextField()
+
+	def __str__(self):
+		return self.title
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
+
+	def summary(self):
+		if len(self.body) > 100:
+			return self.body[:100]+' ...'
+		else:
+			return self.body
